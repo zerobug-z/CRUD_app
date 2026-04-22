02549359c31249c57c7776e5fd5a132ed7a2cd68
anthony80433 <92542098+anthony80433@users.noreply.github.com>
2026-04-22 18:36:25 +0300

    feat: initialize Spring Boot project

---
+++ src/main/java/com/zerobugz/lab_sample_manager/LabSampleManagerApplication.java
@@ -0,0 +1,13 @@
+package com.zerobugz.lab_sample_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSampleManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSampleManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobugz/lab_sample_manager/LabSampleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobugz.lab_sample_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSampleManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
